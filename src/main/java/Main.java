cd18d0b5fe7d8ba5027f503df00f82a72de60a8b
ysiman <isiman@yandex.ru>
2020-08-23 19:00:46 +0300

    yanis v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
 public class Main {
-    String version = "Main v1";
+    String version = "Main v2";
     public static void main(String[] args) {
-        System.out.println("HelloWorld1" );
+        System.out.println("HelloWorld2" );
     }
 }
